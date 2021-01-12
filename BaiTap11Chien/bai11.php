f3f44cf6569a7b1fead3785e103bbb3e815182f0
ellamama123 <67947014+ellamama123@users.noreply.github.com>
2021-01-12 09:37:58 +0700

    Update bai11.php

--- BaiTap11Chien/bai11.php
+++ BaiTap11Chien/bai11.php
@@ -40,20 +40,22 @@ class Product {
     
 
 }
-$product = array(
-    0 => new Product("CPU",750,10,1),
-    1 => new Product("RAM",50,2,2),
-    2 => new Product("HDD",70,1,2),
-    3 => new Product("Main",400,3,1),
-    4 => new Product("Keyboadrd",40,8,4),
-    5 => new Product("Mouse",25,50,4),
-    6 => new Product("VGA",60,3,3),
-    7 => new Product("Monitor",120,28,2),
-    8 => new Product("Case",120,28,5),
+$products = array(
+    new Product("CPU",750,10,1),
+    new Product("RAM",50,2,2),
+    new Product("HDD",70,1,2),
+    new Product("Main",400,3,1),
+    new Product("Keyboadrd",40,8,4),
+    new Product("Mouse",25,50,4),
+    new Product("VGA",60,3,3),
+    new Product("Monitor",120,28,2),
+    new Product("Case",120,28,5),
 );
 function sortByPrice($listProduct){
-    for($i = 0; $i < count($listProduct) - 1; $i++){
-        for ($j = $i + 1; $j < count($listProduct); $j++){
+    $countProduct = count($listProduct);
+    $countList = count($listProduct);
+    for($i = 0; $i < $countProduct - 1; $i++){
+        for ($j = $i + 1; $j < $countList; $j++){
             if($listProduct[$i]->getPrice() > $listProduct[$j]->getPrice()){
                 $tmp = $listProduct[$j];
                 $listProduct[$j] = $listProduct[$i];
@@ -63,6 +65,6 @@ function sortByPrice($listProduct){
     }
     return $listProduct;
 }
-var_dump(sortByPrice($product));
+var_dump(sortByPrice($products));
 
 ?>
