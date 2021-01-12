1482ba5a77d0aede35f1226133b05843abf89f7c
ellamama123 <67947014+ellamama123@users.noreply.github.com>
2021-01-12 09:38:28 +0700

    Update bai12.php

--- BaiTap12Chien/bai12.php
+++ BaiTap12Chien/bai12.php
@@ -40,19 +40,20 @@ class Product {
     
 
 }
-$product = array(
-    0 => new Product("CPU",750,10,1),
-    1 => new Product("RAM",50,2,2),
-    2 => new Product("HDD",70,1,2),
-    3 => new Product("Main",400,3,1),
-    4 => new Product("Keyboadrd",40,8,4),
-    5 => new Product("Mouse",25,50,4),
-    6 => new Product("VGA",60,3,3),
-    7 => new Product("Monitor",120,28,2),
-    8 => new Product("Case",120,28,5),
+$products = array(
+    new Product("CPU",750,10,1),
+    new Product("RAM",50,2,2),
+    new Product("HDD",70,1,2),
+    new Product("Main",400,3,1),
+    new Product("Keyboadrd",40,8,4),
+    new Product("Mouse",25,50,4),
+    new Product("VGA",60,3,3),
+    new Product("Monitor",120,28,2),
+    new Product("Case",120,28,5),
 );
 function sortByName($listProduct)  { 
-    for ($i = 0; $i < count($listProduct); $i++)
+    $countList = count($listProduct);
+    for ($i = 0; $i < $countList; $i++)
     {       
         $loop = $i; 
         $current = $listProduct[$i];
@@ -67,6 +68,6 @@ function sortByName($listProduct)  {
   
     return $listProduct;
 }
-var_dump(sortByName($product));
+var_dump(sortByName($products));
 
 ?>
