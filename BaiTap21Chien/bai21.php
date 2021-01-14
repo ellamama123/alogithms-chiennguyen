74c88fd2b2f7cb0e468a7fd153f2b8e40cce77e0
ellamama123 <67947014+ellamama123@users.noreply.github.com>
2021-01-14 18:06:52 +0700

    Update bai21.php

--- BaiTap21Chien/bai21.php
+++ BaiTap21Chien/bai21.php
@@ -1,14 +1,14 @@
 <?php 
 // sử dụng đệ quy
 function calSalary($salary , $n){
-    if($n==0) return $salary ;
+    if($n==1) return $salary ;
     return calSalary($salary,$n-1) + calSalary($salary,$n-1)*0.1;
 }
 print(calSalary(100,3));
 // không sử dụng đệ quy
 function calSalary1($salary,$n)
 {
-    for($i =1 ; $i<=$n;$i++)
+    for($i=1;$i<=$n;$i++)
     {
         $salary += $salary*0.1;
     }
